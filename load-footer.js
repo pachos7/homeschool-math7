ea8b2cfc6bea6d5ba4f68e021d0ea3224931270d
Pachos7 <francisco.nino@experian.com>
2026-04-28 10:20:36 -0400

    Im super

--- load-footer.js
+++ load-footer.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
         <p>Designed in hell by:</p>
         <p>ILONA MATH-WITCH - A Lab-rat who tests this project (it has made her explote a couple of times......)<br>
             Mango Melon Maracuya - Lab-rat<br>
-            Uncle Pato - A VERY Mad Scientist who is actually a DUCK!</p>
+            Uncle Pato - A VERY Mad Scientist who is actually a Super DUCK!</p>
     </div>
 </footer>`;
     const placeholder = document.getElementById('footer-placeholder');
